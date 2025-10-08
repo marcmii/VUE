cf1ed5a4f8154079df74319cbbfa3dcd152abcbc
marc <2005marcmc@gmail.com>
2025-10-08 18:09:57 +0200

    commit abans de liarla

--- vuetify1/src/pages/afegir.vue
+++ vuetify1/src/pages/afegir.vue
@@ -1,29 +1,91 @@
 <script setup>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const props = defineProps({
-  tasques: Array
+  tasques: {
+    type: Array,
+    default: () => []
+  }
 })
 
 const router = useRouter()
 
-function afegirTasca() {
-  const tasca = prompt("Escriu una tasca")
-  if (tasca) {
-    if (!props.tasques.includes(tasca)) {
-      props.tasques.push(tasca)
-      alert("Tasca creada correctament!")
-      
-    } else {
-      alert("Aquesta tasca ja existeix!")
-    }
+// Nova tasca
+const novaTasca = ref({
+  titol: '',
+  descripcio: '',
+  estat: false
+})
+
+const errors = ref({})
+
+function validarFormulari() {
+  errors.value = {}
+  if (!novaTasca.value.titol.trim()) {
+    errors.value.titol = 'El títol no pot estar buit'
+    return false
   }
+  return true
+}
+
+function afegirTasca() {
+  if (!validarFormulari()) return
+
+  // Afegeix la nova tasca a la llista (si no existeix)
+  const jaExisteix = props.tasques.find(
+    t => t.titol.toLowerCase() === novaTasca.value.titol.toLowerCase()
+  )
+
+  if (jaExisteix) {
+    alert('Aquesta tasca ja existeix!')
+    return
+  }
+
+  props.tasques.push({
+    titol: novaTasca.value.titol,
+    descripcio: novaTasca.value.descripcio,
+    estat: novaTasca.value.estat ? 'completada' : 'pendent'
+  })
+
+  // Reinicia formulari
+  novaTasca.value = { titol: '', descripcio: '', estat: false }
+  alert('Tasca creada correctament!')
 }
 </script>
 
 <template>
   <v-container class="fill-height flex-column justify-center align-center">
-    <h1>Afegir Tasca</h1>
-    <v-btn color="primary" @click="afegirTasca">Crea Tasca</v-btn>
+    <h1 class="mb-4">Afegir Tasca</h1>
+
+    <!-- Formulari dins un v-card -->
+    <v-form
+      class="d-flex flex-column gap-4"
+      style="max-width: 400px; width: 100%;"
+      @submit.prevent="afegirTasca"
+    >
+      <v-text-field
+        v-model="novaTasca.titol"
+        label="Títol"
+        :error-messages="errors.titol"
+        required
+      ></v-text-field>
+
+      <v-textarea
+        v-model="novaTasca.descripcio"
+        label="Descripció (opcional)"
+        rows="2"
+      ></v-textarea>
+
+      <v-switch
+        v-model="novaTasca.estat"
+        label="Completada?"
+        color="success"
+        inset
+      ></v-switch>
+
+      <v-btn type="submit" color="primary" block>Afegir Tasca</v-btn>
+    </v-form>
   </v-container>
 </template>
+
--- vuetify1/src/pages/visualitzar.vue
+++ vuetify1/src/pages/visualitzar.vue
@@ -1,26 +1,46 @@
 <script setup>
-const { tasques } = defineProps({
-  tasques: Array
-})
+    const { tasques } = defineProps({
+        tasques: Array
+    })
 
 
-function editarTasca(element){
-    
-}
+    function editarTasca(index){
+        const tasca = prompt("Edita la tasca")
+        props.tasques.push(tasca[index])
+    }
 
+    function eliminarTasca(index){
+        tasques.splice(index,1)
+    }
 </script>
 
 <template>
-  <v-container class="fill-height flex-column justify-start align-center">
-    <h1>Visualitzar Tasques</h1>
-    <v-list>
-      <v-list-item v-for="(tasca, index) in tasques" :key="index">
-        <v-list-item-content>
-          <v-list-item-title>{{ tasca }}</v-list-item-title>
-          <v-btn color="warning" size="x-small" @click="editarTasca(index)"><v-icon left>mdi-pencil</v-icon>Edita</v-btn>
+  <v-container class="fill-height d-flex flex-column align-center justify-start">
+    <!-- Títol centrat -->
+    <h1 class="text-center mb-6">Visualitzar Tasques</h1>
 
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
+
+    <!-- Llista de tasques -->
+    <v-row class="d-flex flex-row flex-wrap justify-start" dense>
+      <v-col
+        v-for="(tasca, index) in tasques"
+        :key="index"
+        cols="auto"
+        class="mb-4"
+      >
+        <v-card class="pa-4" outlined>
+          <v-card-title class="text-h6">{{ tasca.titol }}</v-card-title>
+          <v-card-title class="text-h6">{{ tasca.descripcio }}</v-card-title>
+          <v-card-title class="text-h6">{{ tasca.estat }}</v-card-title>
+
+          <v-card-actions>
+            <v-btn color="primary" size="small" @click="editarTasca(index)">
+              <v-icon left>mdi-pencil</v-icon>Edita
+            </v-btn>
+            <v-btn color="warning" size="small" @click="eliminarTasca(index)">❌</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
